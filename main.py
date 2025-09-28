287bcb08211fb9bf568c57c1432cf6e024df6719
o_w <o.wiltshire@gmail.com>
2025-09-28 12:57:15 +0700

    fixed, removed the user input and only accept it from command line

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ def main():
         user_input = sys.argv[1]  # First argument after script name
     else:
         print("No variable provided")
-        user_input = sys.stdin.readline().strip()
+        #user_input = sys.stdin.readline().strip()
+        exit(1)
 
     response = client.models.generate_content(
     model='gemini-2.0-flash-001', contents=user_input)
@@ -26,6 +27,6 @@ def main():
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         print(f"Total tokens: {response.usage_metadata.total_token_count}")
-
+        exit(0)
 if __name__ == "__main__":
     main()
